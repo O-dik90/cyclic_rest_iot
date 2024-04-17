65fec9787ff4774d753a694f986702f51f0a34c4
o-dik90 <odikyudi69@gtmail.com>
2024-04-17 20:49:09 +0700

    update models

--- models/relay.js
+++ models/relay.js
@@ -14,6 +14,30 @@ const RelaySchema = new Schema({
     type: String,
     require: false
   },
+  load_1: {
+    type: String,
+    require: false
+  },
+  load_2: {
+    type: String,
+    require: false
+  },
+  load_3: {
+    type: String,
+    require: false
+  },
+  time_1: {
+    type: Date,
+    require: false
+  },
+  time_2: {
+    type: Date,
+    require: false
+  },
+  time_3: {
+    type: Date,
+    require: false
+  },
 })
 
 module.exports = mongoose.model('relay', RelaySchema)
