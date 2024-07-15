9626912da5447ac24fac4d4f9c65c73d2c6b2d22
O-dik90 <odikyudi69@gmail.com>
2024-07-15 20:39:44 +0700

    add method update table

--- index.js
+++ index.js
@@ -268,16 +268,16 @@ app.delete('/temp-delete/:id', async (req, res) => {
 
 
 //** Routing Table */
-app.get('/table-get', async( _, res) => {
+app.get('/table-get', async (_, res) => {
   try {
     const table = await Table.find()
-    
+
     if (!table) {
-      res.status(200).json({message: "data not found"})
+      res.status(200).json({ message: "data not found" })
     }
     res.status(200).json(table)
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
   }
 })
 
@@ -296,11 +296,35 @@ app.delete('/table-delete/:id', async (req, res) => {
     const table = await Table.findByIdAndDelete(id)
 
     if (!table) {
-      res.status(202).json({ message: "data not found" })
+      res.status(404).json({ message: "data not found" })
     }
     res.status(200).json({ message: "success delete item" })
   } catch (error) {
     res.json(500).json({ message: error.message })
+  }
+})
+
+app.put('/table-update/:id', async (req, res) => {
+  const id = { _id: req.params.id }
+  const params = {
+    "first_name": req.body.first_name,
+    "date": req.body.date,
+    "email": req.body.email,
+    "last_name": req.body.last_name,
+    "phone": req.body.phone,
+    "role": req.body.role,
+    "status": req.body.status,
+    "web": req.body.web
+  }
+
+  try {
+    const table = await Table.findByIdAndUpdate(id, params, { new: true })
+    if (!table) {
+      table.status(404).json({ message: "data not found" })
+    }
+    res.status(200).json(table)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
 })
 
