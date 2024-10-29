0779250ce8bd31c1069a2acd9bed73a96f4a3d1e
O-dik90 <odikyudi69@gmail.com>
2024-10-29 20:45:29 +0700

    update logic CRUD file uploader

--- controller/file.js
+++ controller/file.js
@@ -2,7 +2,6 @@ const multer = require("multer");
 const path = require('path');
 const { format, nextDay } = require('date-fns');
 const File = require('../models/file');
-const file = require("../models/file");
 
 const now = new Date();
 const formattedDate = format(now, 'yyyy_MM_dd_HH:mm:ss');
@@ -16,37 +15,20 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: (50 * 1024 * 1024 ) } // 50 MB file size limit
+  limits: { fileSize: (50 * 1024 * 1024) } // 50 MB file size limit
 }).single('file');
 
 const filesAdd = async (req, res) => {
   try {
-    upload(req, res, async (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'File upload error', details: err });
-      }
-      if (!req.file) {
-        return res.status(400).json({ error: 'Please provide a file' });
-      }
-
-      // Create a new document in MongoDB for the uploaded file
-      const file = new File({
-        name: req.file.originalname,
-        size: req.file.size,
-        type: req.file.mimetype,
-        uploadDate: new Date(),
-      });
-
-      try {
-        // Save the file document to MongoDB
-        await file.save();
-        res.send({ message: 'success upload file', file });
-      } catch (dbError) {
-        console.error(dbError);
-        res.status(500).send({ message: 'Error saving file information to the database' });
-      }
+    const file = new File({
+      name: req.file.originalname,
+      size: req.file.size,
+      type: req.file.mimetype,
+      uploadDate: new Date(),
     });
+
+    await file.save();
+    res.status(200).json({ message: 'success upload file', file });
   } catch (error) {
     res.status(500).json({ message: 'File upload failed', error });
   }
@@ -55,19 +37,56 @@ const filesAdd = async (req, res) => {
 const filesGet = async (req, res) => {
   try {
     const files = await File.find();
-    
-    if (!files) 
+
+    if (!files)
       throw new Error('Empty files')
-    
+
     res.status(200).json(files)
   } catch (error) {
-    res.status(500).json({message: error})
+    res.status(500).json({ message: error })
   }
 }
 
+const filesDelete = async (req, res) => {
+  try {
+    const id = { _id: req.params.id }
+    const deletedFile = await File.findByIdAndDelete(id);
 
+    if (!deletedFile) {
+      return res.status(404).json({ message: 'File not found' });
+    }
+
+    res.status(200).json({ message: 'File deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting file', error });
+  }
+}
+
+const filesUpdate = async (req, res) => {
+  try {
+    console.log(req.file)
+    const id = { _id: req.params.id }
+    const params = {
+      "name": req.file.originalname,
+      "size": req.file.size,
+      "type": req.file.type,
+      "UploadDate": new Date()
+    }
+
+    const updateFile = await File.findByIdAndUpdate(id, params, { new: true })
+
+    if (!updateFile)
+      return res.status(400).json({ messgae: 'File not Found' })
+
+    res.status(200).json(updateFile)
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating file', error });
+  }
+}
 module.exports = {
   upload,
   filesAdd,
-  filesGet
+  filesGet,
+  filesDelete,
+  filesUpdate
 }
--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const filesController = require('../controller/file');
 
 const router = express.Router();
-router.post('/files-upload', filesController.filesAdd);
+router.post('/files-upload', filesController.upload ,filesController.filesAdd);
 router.get('/files-get', filesController.filesGet);
+router.delete('/files-delete/:id', filesController.filesDelete);
+router.put('/files-update/:id', filesController.upload ,filesController.filesUpdate);
 
 module.exports= router;
